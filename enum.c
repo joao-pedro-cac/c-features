559c33e0b2d1d84b39f38dc8a77e004724164803
João Pedro <joaopedrocac@outlook.com>
2023-01-10 16:35:19 -0300

    Updated enum.c

--- enum.c
+++ enum.c
@@ -6,10 +6,10 @@ enum semana {dom, seg, ter, qua, qui, sex, sab};
 // dom = 0
 // seg = 1
 // ter = 2
-// qua = 4
-// qui = 5
-// sex = 6
-// sab = 7
+// qua = 3
+// qui = 4
+// sex = 5
+// sab = 6
 
 enum boolAndOthers{T = 1, F = 0, E, G, H = 4, I};
 
