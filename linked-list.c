dc0dde5d0391a6c104b2dadedee9ebd85fc74416
João Pedro <joaopedrocac@outlook.com>
2023-02-02 18:28:00 -0300

    Added linked-list.c

--- linked-list.c
+++ linked-list.c
@@ -4,9 +4,15 @@
  * O primeiro aponta para o segundo, o segundo para o terceiro, etc.
  * O último nó aponta para NULL
  *
+ *
  * A lista não pode ser acessada diretamente
  * Os nós estão espalhados pela memória heap
  * Amplamente utilizadas na implementação de estruturas de dados como filas, pilhas, grafos, árvores, etc.
+ *
+ *
+ * Existem diversas variações, como listas circulares, listas duplamente ligadas, listas com nó cabeça, listas com nó sentinela, etc.
+ *
+ * LISTAS CIRCULARES: o último
  */
 
 #include <stdio.h>
@@ -14,7 +20,7 @@
 #include <stdbool.h>
 
 
-define TAM sizeof(Node);
+#define TAM sizeof(Node)
 
 
 typedef unsigned int uint;
@@ -22,6 +28,7 @@ typedef int Type;
 typedef struct Node Node;
 
 
+void printList(Node *L);
 bool isEmpty(Node *L);
 uint size(Node *L);
 long getIndex(Node *L, Type elem);
@@ -45,9 +52,13 @@ struct Node {
 
 int main() {
 	Node *L = NULL;                                           // Ponteiro de acesso à lista
-	int del;                                                  // Elemento a ser removido
+	Type del;                                                 // Elemento a ser removido
 
-	printf("ESTADO: %s\n\n", isEmpty(L) ? "VAZIA" : "OK");      // Estado inicial
+	Type elems[] = {521, 0, 23, -1, 5, 3, -41, 99, 101};
+	uint sizeElems = sizeof(elems) / sizeof(Type);
+
+
+	printf("ESTADO: %s\n\n", isEmpty(L) ? "VAZIA" : "OK");    // Estado inicial
 
 	// Inserção na lista
 	insertBegin(&L, 0);
@@ -61,6 +72,72 @@ int main() {
 	insertEnd(&L, 99);
 	insert(&L, -1, 7);
 
+	printf("LISTA : ");                                       // Lista preenchida
+	printList(L);
+
+	printf("Tamanho : %u\n\n", size(L));                      // Tamanho da lista
+
+
+
+	printf("----------------------------------------------------\n\n");
+
+
+
+	for (uint i = 0; i < sizeElems; i++) {
+		uint index = getIndex(L, elems[i]);
+		printf("%-5díndice %-2d %s\n", elems[i], index, index == -1 ? "(inexistente)" : "(encontrado)");
+	}
+
+	printf("\nESTADO: %s\n\n", isEmpty(L) ? "VAZIA" : "OK");  // Estado pós-inserção
+
+
+
+	printf("----------------------------------------------------\n\n");
+
+
+
+	// Remoção na lista
+	printf("DELETANDO INÍCIO:  ");
+	del = deleteBegin(&L);
+	printList(L);
+	printf("Elemento deletado: %d\n\n", del);
+
+	printf("DELETANDO FIM:     ");
+	del = deleteEnd(&L);
+	printList(L);
+	printf("Elemento deletado: %d\n\n", del);
+
+	printf("DELETANDO ÍND 2:   ");
+	del = delete(&L, 2);
+	printList(L);
+	printf("Elemento deletado: %d\n\n", del);
+
+	printf("DELETANDO ÍND 0:   ");
+	del = delete(&L, 0);
+	printList(L);
+	printf("Elemento deletado: %d\n\n", del);
+
+	printf("DELETANDO ÍND 5:   ");
+	del = delete(&L, 5);
+	printList(L);
+	printf("Elemento deletado: %d\n\n", del);
+
+	printf("ESTADO: %s\n\n", isEmpty(L) ? "VAZIA" : "OK");    // Estado pós-remoção
+
+
+
+	printf("----------------------------------------------------\n\n");
+
+
+
+	printf("> Limpando lista...\n\n");
+
+	clear(&L);                                                // Limpeza da lista
+
+	printf("ESTADO: %s\n\n", isEmpty(L) ? "VAZIA" : "OK");    // Estado pós-limpeza (final)
+	printf("LISTA : ");
+	printList(L);
+
 	return 0;
 }
 
@@ -71,6 +148,18 @@ int main() {
 //////////////////////////
 
 
+
+// Impressão dos itens
+void printList(Node *L) {
+	Node *aux = L;
+
+	while (aux) {
+		printf("%d", aux -> info);
+		if (aux -> next) printf(", ");
+		aux = aux -> next;
+	}
+	printf("\n");
+}
 
 // Verificação de vacuidade
 bool isEmpty(Node *L) {
@@ -99,6 +188,19 @@ long getIndex(Node *L, Type elem) {
 		aux = aux -> next;
 	}
 	return -1;
+}
+
+// Número de ocorrências
+uint count(Node *L, Type elem) {
+	uint c = 0;
+	Node *aux = L;
+
+	while (aux) {
+		if (aux -> info == elem) c++;
+		aux = aux -> next;
+	}
+
+	return c;
 }
 
 
